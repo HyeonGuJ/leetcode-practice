b43b43f8415444594b40d0263b679b5cf8a58884
hyeongu.jeon <hyeongu.jeon@nhnpayco.com>
2021-04-08 23:44:50 +0900

    _42583_TruckOnBridge

--- src/programmers/problems/_42583_TruckOnBridge.java
+++ src/programmers/problems/_42583_TruckOnBridge.java
@@ -2,10 +2,42 @@ package programmers.problems;
 
 public class _42583_TruckOnBridge {
 
-    public int solution(int bridge_length, int weight, int[] truck_weights) {
+    int weight = 0;
+    int arrived = 0;
 
-        int[] arrived = new int[truck_weights.length];
-        int answer = 0;
-        return answer;
+    public int solution(int bridgeLength, int maxWeight, int[] trucks) {
+
+        int[] bridge = new int[bridgeLength];
+
+        int time = 0;
+        int index = 0;
+
+        while (arrived < trucks.length) {
+
+            tictoc(bridge);
+            time++;
+
+            if (index >= trucks.length || weight + trucks[index] > maxWeight) {
+                continue;
+            } else {
+                bridge[0] = trucks[index];
+                weight += trucks[index];
+                index++;
+            }
+        }
+
+        return time;
+    }
+
+    void tictoc(int[] bridge) {
+
+        if (bridge[bridge.length - 1] != 0) {
+            arrived++;
+            weight -= bridge[bridge.length - 1];
+        }
+        for (int i = bridge.length - 1; i > 0; i--) {
+            bridge[i] = bridge[i - 1];
+        }
+        bridge[0] = 0;
     }
 }
